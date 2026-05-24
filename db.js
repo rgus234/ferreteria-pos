fe05e180c2b86006b7d0afe222023d5d83db5506
Gustavo Diaz <gustavodiazruiz4@gmail.com>
2026-05-24 12:02:51 -0600

    Corregir conexión PostgreSQL Render

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "ferreteria_olimpico",
-    password: "1234",
-    port: 5432,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: "postgres",
+      host: "localhost",
+      database: "ferreteria_olimpico",
+      password: "1234",
+      port: 5432,
+    });
 
 module.exports = pool;
